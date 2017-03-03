7c538768024ecc3c068ecb641fe8ca0c62f0d816
core23 <mail@core23.de>
2017-03-03 17:48:00 +0100

    Local docker tools

---
+++ docker/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker build --no-cache -t core23/composer      composer
+docker build --no-cache -t core23/dephpend      dephpend
+docker build --no-cache -t core23/php-cs-fixer  php-cs-fixer
+docker build --no-cache -t core23/phpdoc-to-typehint  phpdoc-to-typehint
+docker build --no-cache -t core23/phpstan       phpstan
+docker build --no-cache -t core23/phpunit5      phpunit5
+docker build --no-cache -t core23/phpunit6      phpunit6

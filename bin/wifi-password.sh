35d14c6153e8e0cd01a88684e0752d47747f80f8
core23 <mail@core23.de>
2018-10-05 14:52:44 +0200

    Hardened bash scripts

--- bin/wifi-password.sh
+++ bin/wifi-password.sh
@@ -4,7 +4,7 @@ version="0.1.0"
 
 # locate airport(1)
 airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
-if [ ! -f $airport ]; then
+if [ ! -f ${airport} ]; then
   echo "ERROR: Can't find \`airport\` CLI program at \"$airport\"."
   exit 1
 fi
@@ -35,7 +35,7 @@ EOF
 while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do
   case $1 in
     -V | --version )
-      echo $version
+      echo ${version}
       exit
       ;;
     -q | --quiet )
@@ -58,7 +58,7 @@ if [ "" != "$args" ]; then
   ssid="$@"
 else
   # get current ssid
-  ssid="`$airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'`"
+  ssid="`${airport} -I | awk '/ SSID/ {print substr($0, index($0, $2))}'`"
   if [ "$ssid" = "" ]; then
     echo "ERROR: Could not retrieve current SSID. Are you connected?" >&2
     exit 1
@@ -66,7 +66,7 @@ else
 fi
 
 # warn user about keychain dialog
-if [ $verbose ]; then
+if [ ${verbose} ]; then
   echo ""
   echo "\033[90m … getting password for \"$ssid\". \033[39m"
   echo "\033[90m … keychain prompt incoming. \033[39m"
@@ -77,7 +77,7 @@ sleep 2
 # source: http://blog.macromates.com/2006/keychain-access-from-shell/
 pwd="`security find-generic-password -ga \"$ssid\" 2>&1 >/dev/null`"
 
-if [[ $pwd =~ "could" ]]; then
+if [[ ${pwd} =~ "could" ]]; then
   echo "ERROR: Could not find SSID \"$ssid\"" >&2
   exit 1
 fi
@@ -91,9 +91,9 @@ if [ "" == "$pwd" ]; then
 fi
 
 # print
-if [ $verbose ]; then
+if [ ${verbose} ]; then
   echo "\033[96m ✓ \"$pwd\" \033[39m"
   echo ""
 else
-  echo $pwd
+  echo ${pwd}
 fi

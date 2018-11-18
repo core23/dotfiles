685c3da0f35c15477d563768e2124533dfb4b0cc
core23 <mail@core23.de>
2018-11-18 16:16:02 +0100

    Updated docker tools

--- docker/build.sh
+++ docker/build.sh
@@ -9,4 +9,5 @@ docker build --no-cache -t core23/phpdoc-to-typehint        phpdoc-to-typehint
 docker build --no-cache -t core23/phpstan                   phpstan
 docker build --no-cache -t core23/phpunit5                  phpunit5
 docker build --no-cache -t core23/phpunit6                  phpunit6
+docker build --no-cache -t core23/phpunit7                  phpunit7
 docker build --no-cache -t core23/rmt                       rmt

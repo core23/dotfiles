f5e1f970bc7485fbd56b014bbd8710d805f1daca
core23 <mail@core23.de>
2020-12-09 20:30:57 +0100

    Update brew tools

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ backup() {
 
 install() {
     echo "Installing brew"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     echo "Installing brew modules"
     echo #

6a32d3912ae24eb0d6197a57855fb69ce1ef9b99
core23 <mail@core23.de>
2020-12-09 20:00:29 +0100

    Remove docker config (unless there is support for arm)

--- docker/install-daemon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo cp -f de.core23.socat_launcher.plist /Library/LaunchDaemons
-
-cd /Library/LaunchDaemons/
-sudo chown root:wheel de.core23.socat_launcher.plist
-sudo launchctl load de.core23.socat_launcher.plist
-
-echo "Docker TCP daemon installed"
